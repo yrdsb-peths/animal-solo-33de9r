bc5b8bbafb11ec1501c34f739de1da40d402e2a7
33de9r <chenjiayu2033@gmail.com>
2025-04-28 20:25:11 -0400

    New Hero Class Elaphant added to World.

---
+++ Elephant.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * The Elephant, our hero.
+ * @author (your name) Jiayu
+ * @version (a version number or a date) April 28
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Elephant() {
+        setImage("images/Elephant.png");
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+    }
+}
