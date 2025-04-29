439a36745da38a2f61e741316513df239eb77e26
33de9r <chenjiayu2033@gmail.com>
2025-04-29 17:49:38 -0400

    Updated Apple random generator mistake.

--- MyWorld.java
+++ MyWorld.java
@@ -14,6 +14,6 @@ public class MyWorld extends World {
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
         int y = Greenfoot.getRandomNumber(300);
-        addObject(apple, 300, 0);
+        addObject(apple, x, y);
     }
 }
