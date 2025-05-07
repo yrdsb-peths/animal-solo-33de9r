b98c3a24e1746c26546a82a527a49fdc86b617d6
33de9r <chenjiayu2033@gmail.com>
2025-05-07 14:28:41 -0400

    Added facing left and right.

--- Elephant.java
+++ Elephant.java
@@ -9,19 +9,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage[] idle = new GreenfootImage[8];
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    String facing = "right";
     
     public Elephant() {
-        for (int i = 0; i < idle.length; i++) {
-            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        for (int i = 0; i < idleRight.length; i++) {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
         }
-        setImage(idle[0]);
+        setImage(idleRight[0]);
+        
+        for (int i = 0; i < idleLeft.length; i++) {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        setImage(idleLeft[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant() {
-        setImage(idle[imageIndex]);
-        imageIndex = (imageIndex + 1) % idle.length;
+        if (facing.equals("right")) {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        } else {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
     }
     
     public void act()
@@ -29,8 +45,10 @@ public class Elephant extends Actor
         // Add your action code here.
         if (Greenfoot.isKeyDown("left")) {
             move(-1);
+            facing = "left";
         } else if (Greenfoot.isKeyDown("right")) {
             move(1);
+            facing = "right";
         }
         
         eat();
