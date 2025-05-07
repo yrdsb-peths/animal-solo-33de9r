01ec7bd52af4f7f265462c7ac659f9d6264481db
33de9r <chenjiayu2033@gmail.com>
2025-05-07 15:20:55 -0400

    update title position

--- TitleScreen.java
+++ TitleScreen.java
@@ -18,7 +18,7 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        addObject(titleLabel, getWidth()/2, 200);
+        addObject(titleLabel, 250, 100);
         prepare();
     }
 
@@ -37,5 +37,15 @@ public class TitleScreen extends World
         Elephant elephant = new Elephant();
         addObject(elephant,492,82);
         elephant.setLocation(495,86);
+        Label label = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label, 220, 257);
+        label.setLocation(297, 243);
+        label.setLocation(305, 240);
+        Label label2 = new Label("Press <space> to Start", 40);
+        addObject(label2, 249, 331);
+        label2.setLocation(326, 319);
+        label.setLocation(328, 253);
+        label2.setLocation(295, 326);
+        label.setLocation(278, 261);
     }
 }
